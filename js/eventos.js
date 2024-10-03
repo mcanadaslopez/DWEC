0ce1b1ad070fb74d81fb858db5834bdb4e895da6
marcelo.canadas <marcelo.canadaslopez@gmail.com>
2024-10-03 12:51:03 +0200

    Con Dom cargado

--- js/eventos.js
+++ js/eventos.js
@@ -1,2 +1,24 @@
 'use strict';
-window.alert("Eventos.js");
+document.addEventListener("DOMContentLoaded", () => {
+        
+    console.log("Atrapamos evento de onclick en foto");
+
+    function minimizar(){
+        const f = document.getElementById("foto");
+        f.innerHTML="Camba";
+        //f.style.height="15px";
+        //f.style.width="15px";
+        //f.style.backgroundColor="Red";
+    };
+
+    var xa=document.getElementById("foto");//addEventListener("click",minimizar,true);
+    var xafoto=document.getElementById("b1").addEventListener("click",minimizar,false);
+    xa.addEventListener ("mouseover" , minimizar , false);
+});
+
+
+
+
+
+
+
